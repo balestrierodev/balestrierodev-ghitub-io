65cb0a2014359be5387553c318194d7c038d90ae
Gianfilippo Balestriero <balestrierodev@gmail.com>
2022-09-17 01:20:46 +0200

    update repo url

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -4,7 +4,7 @@ export const config = {
         title: "Gianfilippo Balestriero",
         subtitle: "React / Angular / Node"
     },
-    repo: "https://github.com/balestrierodev/balestrierodev.github.io",
+    repo: "https://github.com/balestrierodev/balestrierodev-ghitub-io",
     readme: "https://raw.githubusercontent.com/balestrierodev/balestrierodev.github.io/main/readme.md",
     tips: "https://raw.githubusercontent.com/balestrierodev/balestrierodev.github.io/main/tips.md",
     questions: "https://raw.githubusercontent.com/balestrierodev/balestrierodev.github.io/main/questions.md",
